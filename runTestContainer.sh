56578cb0b4f26d34cad0e59adfbe609d85df9eda
BerislavV <berislav.vidakovic@gmail.com>
2026-01-10 10:36:18 +0100

    Test env. containerization cleanup

--- runTestContainer.sh
+++ runTestContainer.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
+# --- Backend container -------------------------------------------
+
 # Stop and remove any existing container
 docker rm -f chatapp-backend-test >/dev/null 2>&1 || true
 
 # Build Docker image from Dockerfile
-docker build -t chatapp-backend-test .
+docker build -t chatapp-backend-test ./backend/
 
 # Run container
 docker run -d \
@@ -21,3 +23,28 @@ docker run -d \
   java -jar app.jar --server.port=8080
 
 echo "Container 'chatapp-backend-test' is running on Port 8085"
+
+# --- Frontend container -------------------------------------------
+IMAGE_NAME=chatapp-frontend-test
+CONTAINER_NAME=chatapp-frontend-test
+HOST_PORT=8086
+CONTAINER_PORT=80
+
+echo "Stopping and removing existing frontend test container (if any)..."
+docker rm -f $CONTAINER_NAME >/dev/null 2>&1 || true
+
+echo "Building frontend test Docker image..."
+docker build -t $IMAGE_NAME ./frontend/
+
+echo "Running frontend test container..."
+docker run -d \
+  --name $CONTAINER_NAME \
+  -p $HOST_PORT:$CONTAINER_PORT \
+  --restart unless-stopped \
+  $IMAGE_NAME
+
+echo "Frontend Test container is running on port $HOST_PORT"
+
+
+
+
